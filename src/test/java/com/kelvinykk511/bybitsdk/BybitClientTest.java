ac910587028c296f35f574f0c4b675ab6fef7aca
Kelvin Yang <kelvinykk511@gmail.com>
2024-03-26 15:43:49 +0800

    remove api key

--- src/test/java/com/kelvinykk511/bybitsdk/BybitClientTest.java
+++ src/test/java/com/kelvinykk511/bybitsdk/BybitClientTest.java
@@ -11,8 +11,8 @@ public class BybitClientTest {
 
     @Test
     public void test() throws IOException {
-        String apiKey = "1XD0jf0MXPUq5WgYDm";
-        String secret = "DmzflkK41KlxxBWksedPH3tZYv5PTi2VhzfK";
+        String apiKey = "1";
+        String secret = "1";
 
         BybitClient bybitClient = new BybitClient(apiKey, secret, true);
         RealtimeOrderReq req = RealtimeOrderReq.builder()
